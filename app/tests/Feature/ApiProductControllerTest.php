6d99da612c3ab6011f4adad7f2af34dd56b80d17
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-11-30 23:13:46 -0500

    added AAA and PPP labels to tests

--- app/tests/Feature/ApiProductControllerTest.php
+++ app/tests/Feature/ApiProductControllerTest.php
@@ -25,30 +25,43 @@ class ApiProductControllerTest extends TestCase
     #[Test]
     public function api_product_route_should_return_200_success(): void
     {
+        // Arrage / Prepare
+
+        // Act / Perform
         $response = $this->getJson(route('products.index'));
 
+        // Assert / Predict
         $response->assertOk();
     }
 
     #[Test]
     public function api_product_route_should_return_201_created(): void
     {
+        // Arrage / Prepare
+
+        // Act / Perform
         $response = $this->postJson(route('products.store'), $this->product);
 
+        // Assert / Predict
         $response->assertCreated();
     }
 
     #[Test]
     public function api_product_route_should_return_422_error(): void
     {
+        // Arrage / Prepare
+
+        // Act / Perform
         $response = $this->postJson(route('products.store'), []);
 
+        // Assert / Predict
         $response->assertUnprocessable();
     }
 
     #[Test]
     public function api_product_route_should_update_and_return_204_updated(): void
     {
+        // Arrage / Prepare
         $updatedProduct = $this->product;
         $updatedProduct['description'] = 'PHPUNIT_TEST_PRODUCT_DESCRIPTION';
 
@@ -58,43 +71,61 @@ class ApiProductControllerTest extends TestCase
         $json = $response->json();
         $id = $json['data']['id'];
 
+        // Act / Perform
         $response = $this->putJson(route('products.update', $id), $updatedProduct);
 
+        // Assert / Predict
         $response->assertNoContent();
     }
 
     #[Test]
     public function api_product_route_should_remove_and_return_204_deleted(): void
     {
+        // Arrage / Prepare
         $response = $this->postJson(route('products.store'), $this->product);
         $json = $response->json();
         $id = $json['data']['id'];
 
+        // Act / Perform
         $response = $this->deleteJson(route('products.destroy', $id));
+        //
+        // Assert / Predict
         $response->assertNoContent();
     }
 
     #[Test]
     public function api_product_route_should_not_remove_and_return_404(): void
     {
+        // Arrage / Prepare
+
+        // Act / Perform
         $response = $this->deleteJson(route('products.destroy', 999));
+
+        // Assert / Predict
         $response->assertNotFound();
     }
 
     #[Test]
     public function api_products_route_should_return_200_if_product_found(): void
     {
+        // Arrage / Prepare
         $response = $this->postJson(route('products.store'), $this->product);
         $response->assertCreated();
 
+        // Act / Perform
         $response = $this->getJson(route('products.show', 1));
+        // Assert / Predict
         $response->assertOK();
     }
 
     #[Test]
     public function api_products_route_should_return_404_if_product_not_found(): void
     {
+        // Arrage / Prepare
+
+        // Act / Perform
         $response = $this->getJson(route('products.show', 1));
+        // Assert / Predict
         $response->assertNotFound();
     }
 }
